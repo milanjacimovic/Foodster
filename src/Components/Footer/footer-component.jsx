c1660b3c65a7f6452e4cc4a5c8e7dcf1463870a3
milanjacimovic <jacimovicmilanfon@gmail.com>
2021-01-23 18:24:02 +0100

    Footer corrections

--- src/Components/Footer/footer-component.jsx
+++ src/Components/Footer/footer-component.jsx
@@ -5,7 +5,22 @@ import "./footer.styles.scss";
 const Footer = () => (
   <footer className="footer" id="footer">
     <div className="footer-row">
-      <div className="footer-row-col1">Copyright © 2021 by Milan Jaćimović.</div>
+      <div className="footer-row-col1">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#meals">Meals</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
       <div className="footer-row-col2">
         <ul>
           <li>
@@ -31,6 +46,7 @@ const Footer = () => (
         </ul>
       </div>
     </div>
+    <div className="footer-about">Copyright © 2021 by Milan Jaćimović.</div>
     <div className="download">
       <a
         className="btn btn-white btn-animated"
